8cd6d6248c2a31e591bddf8157df167c84f17275
nfagan <nick@fagan.org>
2018-03-08 12:35:11 -0500

    update instances

--- api/matlab/loc_instances.m
+++ api/matlab/loc_instances.m
@@ -1,14 +1,25 @@
-function instances = loc_instances()
+function instances = loc_instances(index)
 
 %   LOC_INSTANCES -- Return ids of all active locators.
 %
+%     instances = loc_instances(); returns the ids of all active locators.
+%     instance = loc_instances(1); returns the first id, if it exists.
+%
 %     See also loc_create, loc_destroy
 %
+%     IN:
+%       - `index` (uint32) |OPTIONAL| -- Subset of locator ids to return.
 %     OUT:
 %       - `instances` (uint32) -- Handle(s) to active locators.
 
 op_code = loc_opcodes( 'instances' );
 
 instances = loc_api( op_code );
+
+if ( nargin == 0 )
+  return;
+end
+
+instances = instances( index );
 
 end
