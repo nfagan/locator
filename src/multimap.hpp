7e161a9e4269f0c12fe3b470926c61854b307fcb
nfagan <nick@fagan.org>
2018-03-12 21:14:37 -0400

    add multimap

---
+++ src/multimap.hpp
@@ -0,0 +1,13 @@
+//
+//  multimap.hpp
+//  bit_array-test
+//
+//  Created by Nick Fagan on 3/12/18.
+//
+
+#ifndef multimap_hpp
+#define multimap_hpp
+
+#include <stdio.h>
+
+#endif /* multimap_hpp */
